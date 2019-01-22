1f0c2187158c3f087c5fda5b22ad9c8155af1eac
sanjit69 <sanjitkatwal@gmail.com>
2019-01-22 19:26:17 +0545

    table related operations and views are done

---
+++ app/Repositories/QrcodeRepository.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Qrcode;
+use InfyOm\Generator\Common\BaseRepository;
+
+/**
+ * Class QrcodeRepository
+ * @package App\Repositories
+ * @version January 22, 2019, 1:07 pm UTC
+ *
+ * @method Qrcode findWithoutFail($id, $columns = ['*'])
+ * @method Qrcode find($id, $columns = ['*'])
+ * @method Qrcode first($columns = ['*'])
+*/
+class QrcodeRepository extends BaseRepository
+{
+    /**
+     * @var array
+     */
+    protected $fieldSearchable = [
+        'user_id',
+        'company_name',
+        'product_name',
+        'website',
+        'product_url',
+        'callback_url',
+        'qrcode_path',
+        'amount',
+        'status'
+    ];
+
+    /**
+     * Configure the Model
+     **/
+    public function model()
+    {
+        return Qrcode::class;
+    }
+}
